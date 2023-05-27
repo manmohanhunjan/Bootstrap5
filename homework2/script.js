8c90dee3d795540b55f49e12b9fa18e7d3dd1d6a
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-05-27 17:03:02 +0530

    progress 30%

--- homework2/script.js
+++ homework2/script.js
@@ -1,9 +1,41 @@
-const sideNavContainer = document.querySelector('#sideNavContainer');
+// const sideNavContainer = document.querySelector('#sideNavContainer');
 
-sideNavContainer.addEventListener('click', (event) => {
-    if (event.target.parentElement.id == 'selectedIcon') {
-        console.log(event.target.parentElement);
-        let targetedElement = event.target.parentElement;
-        targetedElement.classList.toggle('active');
-    }
+// sideNavContainer.addEventListener('click', (event) => {
+//     if (event.target.parentElement.id == 'selectedIcon') {
+//         console.log(event.target.parentElement);
+//         let targetedElement = event.target.parentElement;
+//         targetedElement.classList.toggle('active');
+//     }
+// });
+
+// const collapseBtn = document.querySelector('#collapseButton')
+// const arrowIcon = document.querySelectorAll('.arrow-icon')
+
+// collapseBtn.addEventListener('click', (event) => {
+//     // index of clicked event
+//     let index = event.target.indexOf
+//     // toggle arrow icon
+//     arrowIcon[index].classList.toggle('bi-chevron-down')
+//     arrowIcon[index].classList.toggle('bi-chevron-up')
+
+// })
+
+const collapseBtns = document.querySelectorAll('[data-bs-toggle="collapse"]');
+const arrowIcons = document.querySelectorAll('.arrow-icon');
+
+collapseBtns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    arrowIcons[index].classList.toggle('bi-chevron-down');
+    arrowIcons[index].classList.toggle('bi-chevron-up');
+    collapseBtns[index].classList.toggle('text-light');
+  });
 });
+
+const sideBarActiveContainer = document.querySelector('#sideBarActiveContainer');
+const sideBarActive = document.querySelector('#sideBarActive');
+
+sideBarActive.addEventListener('click', () => {
+    sideBarActiveContainer.classList.toggle('d-none');
+
+    }
+);
